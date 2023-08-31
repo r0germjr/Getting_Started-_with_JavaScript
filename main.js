31c473bc667a53f26f85b62dddf0bf16b7607f85
Rogério Maia Jr <83840116+r0germjr@users.noreply.github.com>
2023-08-31 14:17:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+// Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+console.log(`Hello world`)
+
+// Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+let numberOne = 5;
+let numberTwo = 8;
+console.log(numberOne + numberTwo)
+
+/*
+Crie um script que declare uma variável e verifique se o seu valor é um número. 
+Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+*/
+let numberOrNot = 'hihi'
+if (typeof numberOrNot == 'number') {
+  console.log(`É um número`)
+} else {
+  console.log(`Não é um número`)
+}
+
+/*
+Crie um script que declare uma variável e verifique se o seu valor é uma string. 
+Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+*/
+let stringOrNot = true
+if(typeof stringOrNot == 'string') {
+  console.log(`É uma string`)
+} else {
+  console.log(`Não é uma string`)
+}
+
+/*
+Crie um script que declare uma variável e verifique se o seu valor é um booleano. 
+Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano". 
+*/
+let booleanOrNot = 85
+if(typeof booleanOrNot == 'boolean') {
+  console.log(`É um booleano`)
+} else {
+  console.log(`Não é um booleano`)
+}
+
+/*
+Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+*/
+let numberThree = 8
+let numberFour = 5
+console.log(numberThree - numberFour)
+
+/*
+Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+*/
+let numberFive = 1
+let numberSix = 3
+console.log(numberFive * numberSix)
+
+/*
+Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+*/
+let numberSeven = 3
+let numberEight = 1
+console.log(numberSeven / numberEight)
+
+/*
+Crie um script que declare uma variável e verifique se o seu valor é um número par. 
+Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+*/
+let itsOddOrNot = 13
+if (itsOddOrNot / 2 == 0) {
+  console.log(`É um número par`)
+} else {
+  console.log(`Não é um número par`)
+}
+
+/*
+Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. 
+Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+*/
+let itsEvenOrNot = 85
+if (itsEvenOrNot / 2 != 0) {
+  console.log(`É um número ímpar`)
+} else {
+  console.log(`Não é um número ímpar`)
+}
